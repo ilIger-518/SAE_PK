e5230fc20c2214ee7ed5a1fd1b8d7076f0556ebc
ilIger-518 <m.switon@apfelwerk.de>
2025-04-01 16:17:13 +0200

    Add every method and streamreader

---
+++ SAE_PK/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace SAE_PK;
+using System.IO;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+       Console.WriteLine("jzfu");
+    }
+
+    public static void DatenSpeichern()
+    {
+        
+    }
+
+    public static void DatenLesen()
+    {
+        List<string> runningBehind = new();
+        StreamReader sr = new StreamReader("C:\\Users\\{Username}\\Desktop\\serverLog.log");
+        
+        
+    }
+
+    public static void DatenVerarbeiten()
+    {
+        
+    }
+}
