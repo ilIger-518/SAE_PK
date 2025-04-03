fef9036b98632b4beb86241013fd67e89d02ce4e
ilIger-518 <m.switon@apfelwerk.de>
2025-04-03 14:43:53 +0200

    method-DatenVerarbeiten: finished

--- SAE_PK/Program.cs
+++ SAE_PK/Program.cs
@@ -7,9 +7,12 @@ class Program
 {
     public static void Main(string[] args)
     {
-       Console.WriteLine("Dieses Programm wertet die log Datei eines Servers aus verarbeitet die relevanten Daten.");
+       List<int> msBehind = new();
+       List<int> ticksBehind = new();
+       Console.WriteLine("Dieses Programm wertet die log Datei eines Servers aus und, speichert die verarbeiteten Daten ab.");
        Console.WriteLine("----------------------------------------------------------------------------------------");
-       DatenVerarbeiten(DatenLesen());
+       (msBehind, ticksBehind) = DatenVerarbeiten(DatenLesen());
+       Console.WriteLine($"{msBehind[2]}" +" "+ $"{ticksBehind[3]}"); //temp
     }
 
     public static void DatenSpeichern()
@@ -33,8 +36,13 @@ class Program
         return runningBehind;
     }
 
-    public static List<string> DatenVerarbeiten(List<string> logs)
+    public static (List<int>, List<int>) DatenVerarbeiten(List<string> logs)
     {
+        int msTimeWoMs = 0;
+        string msTimeWMs = "";
+        List<int> serveroverloadTimeTicks = new();
+        List<int> serveroverloadTimeInMs = new();
+        string[] splittedServeroverloadTime = new string[14]; //ms=10, tick=12
         List<string> serveroverloadLog = new();
         int logCounter = 0;
         while (logCounter < logs.Count)
@@ -45,8 +53,16 @@ class Program
             }
             logCounter++;
         }
-        
-        
-        return;
+        logCounter = 0;
+        while (logCounter < serveroverloadLog.Count)
+        {
+            splittedServeroverloadTime = serveroverloadLog[logCounter].Split(' ');    
+            logCounter++;
+            msTimeWMs = splittedServeroverloadTime[10];
+            msTimeWoMs = int.Parse(msTimeWMs.Substring(0, msTimeWMs.Length - 2));
+            serveroverloadTimeInMs.Add(msTimeWoMs);
+            serveroverloadTimeTicks.Add(int.Parse(splittedServeroverloadTime[12]));
+        }
+        return (serveroverloadTimeInMs, serveroverloadTimeTicks);
     }
 }
